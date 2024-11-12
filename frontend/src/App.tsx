c1d3aff08e7d73470d92dd0996902e660ee2a19d
zhasumit <sumitzha2057@gmail.com>
2024-11-12 13:55:08 +0545

    front-end set

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+export default function App() {
+  return (
+    <>
+      <h1 className="text-3xl  text-red-500">
+        Hello world!
+      </h1>
+      <Button>This is a button</Button>
+    </>
+  )
+}
