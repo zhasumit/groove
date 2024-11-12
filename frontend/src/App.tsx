c4b712b6dff181174c23c892ac676b19d60f8ea4
zhasumit <sumitzha2057@gmail.com>
2024-11-12 18:09:24 +0545

    auth setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
 import { Button } from "./components/ui/button";
 
 export default function App() {
   return (
     <>
-      <h1 className="text-3xl  text-red-500">
-        Hello world!
-      </h1>
-      <Button>This is a button</Button>
+      <header>
+        <SignedOut>
+          <SignInButton>
+            <Button>Sign In</Button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
     </>
   )
 }
